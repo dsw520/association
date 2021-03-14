dba5aedf0591a6271ae0783666ebf5c0387135cc
1004722157@qq.com <luo764363678>
2021-03-14 17:06:34 +0800

    增加全局常量类

---
+++ client/src/main/java/cn/lkq520/utils/ConstantsUtils.java
@@ -0,0 +1,21 @@
+package cn.lkq520.utils;
+
+/**
+ * TODO
+ *
+ * @author Luo
+ * @version 1.0
+ * @date 2021/3/14 1:50
+ */
+public final class ConstantsUtils {
+
+    private ConstantsUtils(){}
+
+    /* 编写格式
+    private static final String DD = "XX";
+
+    public static String getDD() {
+        return DD;
+    }
+    */
+}
