237372dff1590a3d0087d98f5de1b0f6ce89ecf4
1004722157@qq.com <luo764363678>
2021-03-14 17:09:12 +0800

    为前端页面导航添加拦截器

---
+++ client/src/main/java/cn/lkq520/config/ClientMenuConfig.java
@@ -0,0 +1,24 @@
+package cn.lkq520.config;
+
+import cn.lkq520.web.controller.interceptor.MenuInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * TODO
+ *
+ * @author Luo
+ * @version 1.0
+ * @date 2021/3/14 16:40
+ */
+@Configuration
+public class ClientMenuConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration registration = registry.addInterceptor(new MenuInterceptor());
+        registration.addPathPatterns("/*").addPathPatterns("/user/registerView");
+    }
+}
---
+++ client/src/main/java/cn/lkq520/web/controller/interceptor/MenuInterceptor.java
@@ -0,0 +1,28 @@
+package cn.lkq520.web.controller.interceptor;
+
+import cn.lkq520.pojo.Nav;
+import cn.lkq520.utils.ApplicationContextUtils;
+import cn.lkq520.web.service.NavService;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * TODO
+ *
+ * @author Luo
+ * @version 1.0
+ * @date 2021/3/14 16:36
+ */
+public class MenuInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView mav) throws Exception {
+        NavService navService = (NavService) ApplicationContextUtils.getBean("navServiceImpl");
+        List<Nav> navList = navService.getNavWithItem();
+        mav.addObject("navList",navList);
+    }
+}
