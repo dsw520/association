f0f24467c653e582afd82674003ab19c06d84407
dsw520 <1004722157@qq.com>
2021-04-25 22:25:32 +0800

    添加后台管理控制器

---
+++ client/src/main/java/cn/lkq520/web/controller/AdminController.java
@@ -0,0 +1,21 @@
+package cn.lkq520.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * TODO
+ *
+ * @author Luo
+ * @version 1.0
+ * @date 2021/4/25 20:42
+ */
+@Controller
+public class AdminController {
+
+    public String index() {
+
+
+        return "";
+    }
+
+}
